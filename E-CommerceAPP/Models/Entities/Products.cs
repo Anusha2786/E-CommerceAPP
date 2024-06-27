b9ebee034071d2b8204ebe70ad2375b6a0e8fecb
rojag99 <roja.gudavalli@snaddevelopers.com>
2024-06-27 18:21:07 +0530

    to push Products class

---
+++ E-CommerceAPP/Models/Entities/Products.cs
@@ -0,0 +1,11 @@
+﻿namespace E_CommerceAPP.Models.Entities
+{
+    public class Products
+    {
+        public int Product_ID { get; set; }
+        public int Product_Name { get; set; }
+        public string Product_Description { get; set; }
+        public decimal Product_Price { get; set; }
+        public int Category_ID { get; set; }
+    }
+}
