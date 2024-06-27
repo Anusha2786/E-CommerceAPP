6bfdf394b13990953996329c3e726922ca24d53a
rojag99 <roja.gudavalli@snaddevelopers.com>
2024-06-27 17:40:48 +0530

    push 1st

---
+++ E-CommerceAPP/Models/Entities/Categories.cs
@@ -0,0 +1,9 @@
+﻿namespace E_CommerceAPP.Models.Entities
+{
+    public class Categories
+    {
+        public int Category_ID { get; set; }
+        public string Category_Name { get; set; }
+
+    }
+}
---
+++ E-CommerceAPP/Models/Entities/Reviews.cs
@@ -0,0 +1,11 @@
+﻿namespace E_CommerceAPP.Models.Entities
+{
+    public class Reviews
+    {
+        public int Review_ID { get; set; }
+        public int Product_Rating { get; set; }
+        public string Comment { get; set; }
+        public decimal Product_ID { get; set; }
+        public int Customer_ID { get; set; }
+    }
+}
