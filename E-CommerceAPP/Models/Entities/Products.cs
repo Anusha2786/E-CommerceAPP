3c0600036e19daff748a295ed9811c48b8a39ae8
rojag99 <roja.gudavalli@snaddevelopers.com>
2024-06-27 17:12:58 +0530

    created Product class

---
+++ E-CommerceAPP/Models/Entities/Products.cs
@@ -0,0 +1,13 @@
+﻿namespace E_CommerceAPP.Models.Entities
+{
+    public class Products
+    {
+        public int Product_ID { get; set; }
+        public string Product_Name { get; set; }
+        public string Product_Description { get; set; }
+        public decimal Product_Price { get; set; }
+        public int Categoty_ID { get; set; }
+
+
+    }
+}
