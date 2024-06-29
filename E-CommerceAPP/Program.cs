b781f1dbfd878e75dec0e01422d2c729fd1b027d
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-06-29 14:57:36 +0530

    orders

--- E-CommerceAPP/Program.cs
+++ E-CommerceAPP/Program.cs
@@ -10,7 +10,19 @@ builder.Services.AddDbContext<OrderDbContext>(options => options.UseSqlServer(bu
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+
+    {
+        Title="My API",
+        Version = "v1",
+        Description="An api is on Orders"
+        
+    });
+
+}
+    );
 builder.Services.AddAuthentication("MyCookieAuth")
     .AddCookie("MyCookieAuth", options =>
     {
